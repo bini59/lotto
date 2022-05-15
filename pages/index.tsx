b2030c95e10a297e7254569a22cd69979ee12961
myuni <myuni0234@gmail.com>
2022-05-15 15:54:19 +0900

    Feat : Apply index.tsx sass module

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,18 +2,14 @@ import { useState, useEffect } from "react";
 import Navbar from "../component/navbar";
 import Nums from "../component/selectNum";
 
-const style = `
-
-`;
-
 const Index = () => {
     return (
         <>
             <Navbar />
             <Nums />
-            <style jsx>{style}</style>
         </>
     );
+    ``;
 };
 
 export default Index;
