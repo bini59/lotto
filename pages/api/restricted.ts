ff2df2cce69cafdf965b55030ee54992d94dd067
bini59 <myuni0234@gmail.com>
2023-12-08 17:17:24 +0900

    fix : warining message

--- pages/api/restricted.ts
+++ pages/api/restricted.ts
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next"
 
 import { unstable_getServerSession } from "next-auth/next"
-import { authOptions } from "./auth/[...nextauth]"
+import authOptions from "./auth/[...nextauth]"
 
 export default async (req:NextApiRequest, res:NextApiResponse) => {
   const session = await unstable_getServerSession(req, res, authOptions)
